8a2fd8e8baa53076b18d7adc2274579230575aa2
Kévin Chalet <kevinchalet@gmail.com>
2015-09-19 23:38:20 +0200

    Update the authorization provider to return an error message for invalid resource owner credentials

--- src/AureliaDemo/Providers/AuthorizationProvider.cs
+++ src/AureliaDemo/Providers/AuthorizationProvider.cs
@@ -15,36 +15,39 @@
     {
         public async override Task GrantResourceOwnerCredentials(GrantResourceOwnerCredentialsContext context)
         {
-            var username = context.UserName;
-            var password = context.Password;
-
             var userManager = context
                 .HttpContext
                 .RequestServices
                 .GetRequiredService<UserManager<ApplicationUser>>();
 
-            var user = await userManager.FindByNameAsync(username);
-            var isValid = await userManager.CheckPasswordAsync(user, password);
+            var user = await userManager.FindByNameAsync(context.UserName);
 
-            if (isValid)
+            if (!await userManager.CheckPasswordAsync(user, context.Password))
             {
-                var identity = new ClaimsIdentity(OpenIdConnectServerDefaults.AuthenticationScheme);
+                context.Rejected(OpenIdConnectConstants.Errors.InvalidGrant, "Invalid username or password");
 
-                // this automatically goes into the token and id_token
-                identity.AddClaim(ClaimTypes.NameIdentifier, user.UserName);
-
-                // the other claims require explicit destinations
-                identity.AddClaim(ClaimTypes.Name, user.FirstName, "token id_token");
-                identity.AddClaim(ClaimTypes.Surname, user.LastName, "token id_token");
-
-                var principal = new ClaimsPrincipal(identity);
-                context.Validated(principal);
+                return;
             }
+
+            var identity = new ClaimsIdentity(OpenIdConnectServerDefaults.AuthenticationScheme);
+
+            // this automatically goes into the token and id_token
+            identity.AddClaim(ClaimTypes.NameIdentifier, user.UserName);
+
+            // the other claims require explicit destinations
+            identity.AddClaim(ClaimTypes.Name, user.FirstName, "token id_token");
+            identity.AddClaim(ClaimTypes.Surname, user.LastName, "token id_token");
+
+            var principal = new ClaimsPrincipal(identity);
+            context.Validated(principal);
         }
 
         public override Task ValidateClientAuthentication(ValidateClientAuthenticationContext context)
         {
+            // JS applications cannot keep their credentials secret: since this Aurelia demo uses a JS app,
+            // client authentication must be skipped to indicate to the OIDC server that the client cannot be trusted.
             context.Skipped();
+
             return Task.FromResult<object>(null);
         }
 
@@ -53,9 +56,9 @@
             if (!context.Request.IsPasswordGrantType() && !context.Request.IsRefreshTokenGrantType())
             {
                 context.Rejected(
-                error: "unsupported_grant_type",
-                description: "Only authorization code and refresh token grant types " +
-                             "are accepted by this authorization server");
+                    error: "unsupported_grant_type",
+                    description: "Only password and refresh token grant types " +
+                                 "are accepted by this authorization server");
             }
 
             return Task.FromResult<object>(null);
